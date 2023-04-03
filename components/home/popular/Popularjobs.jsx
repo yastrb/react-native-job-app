07467752237511e9b475cd840f9583ff4880f80d
yastrb <uaalena202@gmail.com>
2023-04-03 09:23:35 +0300

    new commit

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 
 import styles from './popularjobs.style'
+import { useState } from 'react';
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from './../../common/cards/popular/PopularJobCard';
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch
+    ("search", {
+      query: "react developer",
+      num_pages: "1",
+    });
+  
+  // const [selectedJob, setSelectedJob] = useState();
+
+  // const handleCardPress = (item) => {
+    
+  // };
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item} />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
---
+++ utils/index.js
@@ -0,0 +1,7 @@
+﻿export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+};
